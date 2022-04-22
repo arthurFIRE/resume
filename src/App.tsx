53e2173ab36f08342d260458a4f8cf4d53dc1630
김기만 <rlarlaks21c@naver.com>
2022-04-22 10:59:09 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import 'antd/dist/antd.css';
+import {Button} from "antd";
+
+const App: React.FC = () => {
+    return <>
+            <div className="App">
+                <Button type="primary">Button</Button>
+            </div>
+        </>
+}
+
+export default App;
+
